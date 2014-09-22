26aefefa022b1af4aae7e9891abd1fe480614d0e
Cheng-Shiang Li <yeshuanova@gmail.com>
2014-09-22 13:30:33 +0800

    Initial Commit

---
+++ design_pattern_examples/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  design_pattern_examples
+//
+//  Created by Peter.Li on 2014/9/22.
+//  Copyright (c) 2014年 Peter.Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
